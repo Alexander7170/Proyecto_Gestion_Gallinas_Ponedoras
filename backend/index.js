c6850a9ee8280b0855dba27e15ba17ac270300c0
unknown <alexandertito121@gmail.com>
2026-08-05 20:39:07 -0300

    Agregue carpeta utils e importe e use cors, #29 ya esta hecha

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 import enviorments from "./src/api/config/enviorments.js";
 const app = express();
+app.use(cors());
 app.listen(enviorments.puerto, ()=>{
     console.log(`Sistema levantado en puerto ${enviorments.puerto}`);
 })
